d734be2a6970291e18aea6b46796a2313a10ad35
Finch-75 <williamchadfinch@gmail.com>
2025-01-07 11:55:39 -0700

    name exercise

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
-    }
+        
+       // Ask for first name and middle and last names. Then have console read and store names as string variables.
+       Console.Write("What is your first name? ");
+       string firstName = Console.ReadLine();
+       Console.Write("What is your middle name? ");
+       string middleName = Console.ReadLine();
+       Console.Write("What is your last name? ");
+       string lastName = Console.ReadLine();
+       Console.WriteLine("");
+       // Display names in format Last name , First name Middle name Last name.
+       Console.WriteLine($"Your name is {lastName}, {firstName} {middleName} {lastName}. ");
+}
 }
