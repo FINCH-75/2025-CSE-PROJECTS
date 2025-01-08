cfc84ac38cbe854ec6c33f1fdfab3b0bbc6169d1
FINCH-75 <williamchadfinch@gmail.com>
2025-01-08 16:46:28 -0700

    exercise 3 done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+    // Use random generator and use 1-100.
+    Random randomGenerator = new Random();
+    int number = randomGenerator.Next(1, 100);
+    
+    // create guess integer and give it a value of 0.
+    int guess = 0;
+
+// create a while loop that uses if and else statements to asks the user to give a number and will only finish when user guesses correctly.
+while (guess != number)
+{
+    Console.Write("What is your guess? ");
+    string userGuess = Console.ReadLine();
+     guess = int.Parse(userGuess);
+
+   
+if (guess > number)
+    {
+        Console.WriteLine("Wrong please guess lower");
+        
     }
+     
+     else if (guess < number)
+     {
+        Console.WriteLine("Wrong please guess higher");
+     }
+
+     else
+     {
+        Console.WriteLine("Good job you guessed it!!");
+     }
+
+}
+
+
+    
+    
+}
+    
 }
