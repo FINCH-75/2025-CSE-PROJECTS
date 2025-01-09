273b939b4bd154337b7633a79ff63eabb0b5fec8
Finch-75 <williamchadfinch@gmail.com>
2025-01-09 00:11:19 -0700

    exercise 4 first try

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,49 @@
-using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+
+    List<int> numbers = new List<int>();
+
+    int num = 1;
+    while (num != 0)
+    {
+        Console.Write("Give me a number. Enter 0 when finished");
+        string userNum =Console.ReadLine();
+        num = int.Parse(userNum);
+        if (num != 0)
+        {
+            numbers.Add(num);
+        }
+        
+        
+
+    }
+
+    //Console.WriteLine(numbers.Count);
+
+    int sum = numbers.Sum();
+    Console.WriteLine($"The sum is {sum}");  
+    
+
+    float average = (float)sum / numbers.Count;
+    Console.WriteLine($"The average is {average}");
+    int max = numbers[0];
+    foreach (int number in numbers)
+{
+    if (number > max)
+    {
+        max = number;
+    }
+    
+}
+    
+Console.WriteLine($"The max is {max}");
+    
+       
     }
 }
