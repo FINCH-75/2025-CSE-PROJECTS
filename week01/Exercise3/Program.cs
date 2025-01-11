e0d318941c19afe69c9208294ae79df6f0bdbca7
FINCH-75 <williamchadfinch@gmail.com>
2025-01-11 02:53:13 -0700

    finished

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,13 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
+        Random randomGenerator = new Random();
+        string answer = "";
+while (answer == "yes")
+{
+
     // Use random generator and use 1-100.
-    Random randomGenerator = new Random();
+    //Random randomGenerator = new Random();
     int number = randomGenerator.Next(1, 101);
     
     // create guess integer and give it a value of 0.
     int guess = 0;
 
+
+Console.WriteLine($"Would you like to play?");
+answer = Console.ReadLine();
+Console.WriteLine($"{number}");
+
+  
 // create a while loop that uses if and else statements to asks the user to give a number and will only finish when user guesses correctly...
 while (guess != number)
 {
@@ -36,6 +47,10 @@ if (guess > number)
      }
 
 }
+  
+}
+//Console.WriteLine("Would you like to play again? yes/no");
+//answer = Console.ReadLine();
 
 
     
