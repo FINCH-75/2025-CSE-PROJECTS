969c7cc2ddac96467373984849fafabe9c14f095
Finch-75 <williamchadfinch@gmail.com>
2025-01-15 23:07:16 -0700

    begun

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,27 @@
+public class Journal
+{
+    public List<Entry>_entries;
+
+
+    public void AddEnty(Entry newEntry)
+    {
+
+    }
+
+    public void DisplayAll()
+    {
+
+    }
+
+    public void SafeToFile(string file)
+    {
+
+    }
+
+    public void LoadFile(string file)
+    {
+        
+    }
+
+
+}
---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,14 @@
+using System.Diagnostics.Contracts;
+
+public class PromptGenerator
+{
+
+    public List<string>_prompts;
+
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+
+
+}
